43b48016594b8b324a724f9f3c3c158bc975b3bb
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-06-10 11:45:28 +0530

    Create Reverse Linked List.cpp

---
+++ Easy/Linked Lists/Reverse Linked List.cpp
@@ -0,0 +1,46 @@
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        if(head==NULL)
+        {
+            return head;
+        }
+        
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* tail = head;
+        
+        while(tail->next)
+        {
+            tail=tail->next;
+        }
+        
+        while(curr)
+        {
+            ListNode* tmp = curr->next;
+            curr->next = prev;
+            prev=curr;
+            curr = tmp;
+        }
+        
+        
+        return tail;       
+        
+    }
+};
