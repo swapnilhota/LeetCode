e4eb1cc8930d3a2c7ae76ae194468e0a5520bebf
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-06-14 21:46:01 +0530

    Create Merge Sorted Array.cpp

---
+++ Easy/Arrays/Merge Sorted Array.cpp
@@ -0,0 +1,48 @@
+/*
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+*/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        int l=m-1;
+        int r=n-1;
+        int i=(m+n)-1;
+        while(l>=0 && r>=0)
+        {
+            if(nums1[l]>nums2[r])
+            {
+                nums1[i] = nums1[l];
+                l--;
+            }
+            else
+            {
+                nums1[i] = nums2[r];
+                r--;
+            }
+            i--;
+        }
+        
+        while(l>=0)
+        {
+            nums1[i] = nums1[l];
+            l--;
+            i--;
+        }
+        
+        while(r>=0)
+        {
+            nums1[i] = nums2[r];
+            r--;
+            i--;
+        }
+        
+        return;
+        
+    }
+};
