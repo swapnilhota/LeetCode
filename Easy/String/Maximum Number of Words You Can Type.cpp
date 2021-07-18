9da304a80144b9f81353331f011fd1b7dbca02b2
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-07-18 10:29:21 +0530

    Create Maximum Number of Words You Can Type.cpp

---
+++ Easy/String/Maximum Number of Words You Can Type.cpp
@@ -0,0 +1,86 @@
+/*
+There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
+
+Given a string text of words separated by a single space (no leading or trailing spaces) and a string brokenLetters of all distinct letter keys that are broken, return the number of words in text you can fully type using this keyboard.
+
+ 
+
+Example 1:
+
+Input: text = "hello world", brokenLetters = "ad"
+Output: 1
+Explanation: We cannot type "world" because the 'd' key is broken.
+Example 2:
+
+Input: text = "leet code", brokenLetters = "lt"
+Output: 1
+Explanation: We cannot type "leet" because the 'l' and 't' keys are broken.
+Example 3:
+
+Input: text = "leet code", brokenLetters = "e"
+Output: 0
+Explanation: We cannot type either word because the 'e' key is broken.
+ 
+
+Constraints:
+
+1 <= text.length <= 104
+0 <= brokenLetters.length <= 26
+text consists of words separated by a single space without any leading or trailing spaces.
+Each word only consists of lowercase English letters.
+brokenLetters consists of distinct lowercase English letters.
+*/
+
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        
+        vector<string> words;
+        string tmp="";
+        
+        for(auto ch: text)
+        {
+            if(ch==' ')
+            {
+                words.push_back(tmp);
+                tmp="";
+            }
+            else
+            {
+                tmp+=ch;
+            }
+        }
+        
+        words.push_back(tmp);
+        
+        int ans=0;
+        
+        for(auto word: words)
+        {
+            int flag=0;
+            for(auto ch: word)
+            {
+                for(auto x: brokenLetters)
+                {
+                    if(ch==x)
+                    {
+                        flag=1;
+                        break;
+                    }
+                }
+                if(flag==1)
+                {
+                    break;
+                }
+            }
+            if(flag==0)
+            {
+                ans++;
+            }
+        }
+        
+        return ans;
+        
+        
+    }
+};
