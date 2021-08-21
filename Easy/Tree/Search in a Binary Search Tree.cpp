08def62c76e16fc8a341decc8a80722fa048ac72
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-08-21 12:58:16 +0530

    Create Search in a Binary Search Tree.cpp

---
+++ Easy/Tree/Search in a Binary Search Tree.cpp
@@ -0,0 +1,72 @@
+/*
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+Example 2:
+
+
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 5000].
+1 <= Node.val <= 107
+root is a binary search tree.
+1 <= val <= 107
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL)
+        {
+            return root;
+        }
+        
+        TreeNode* ans = NULL;
+        helper(root, val, ans);
+        return ans;
+    }
+    
+    void helper(TreeNode* root, int val, TreeNode* &ans)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        if(root->val == val)
+        {
+            ans = root;
+            return;
+        }
+        if(root->val < val)
+        {
+            helper(root->right, val, ans);
+        }
+        else
+        {
+            helper(root->left, val, ans);
+        }
+    }
+};
