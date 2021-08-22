3068d36646f4ca89bdd1b4428cd3166d691010ae
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-08-22 12:29:46 +0530

    Create Largest Number At Least Twice of Others.cpp

---
+++ Easy/Arrays/Largest Number At Least Twice of Others.cpp
@@ -0,0 +1,63 @@
+/*
+You are given an integer array nums where the largest integer is unique.
+
+Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [3,6,1,0]
+Output: 1
+Explanation: 6 is the largest integer.
+For every other number in the array x, 6 is at least twice as big as x.
+The index of value 6 is 1, so we return 1.
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: -1
+Explanation: 4 is less than twice the value of 3, so we return -1.
+Example 3:
+
+Input: nums = [1]
+Output: 0
+Explanation: 1 is trivially at least twice the value as any other number because there are no other numbers.
+ 
+
+Constraints:
+
+1 <= nums.length <= 50
+0 <= nums[i] <= 100
+The largest element in nums is unique.
+*/
+
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        if(nums.size()==1)
+        {
+            return 0;
+        }
+        int idx;
+        int maxTerm=INT_MIN;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i]>maxTerm)
+            {
+                maxTerm = nums[i];
+                idx=i;
+            }
+        }
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i]==maxTerm) continue;
+            if(2*nums[i] > maxTerm)
+            {
+                return -1;
+            }
+        }
+        
+        return idx;
+    }
+};
