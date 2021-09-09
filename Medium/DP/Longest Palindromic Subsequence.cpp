5a3f89133ec7f606940f868b32c1a08f0959b8d6
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-09-09 18:22:05 +0530

    Create Longest Palindromic Subsequence.cpp

---
+++ Medium/DP/Longest Palindromic Subsequence.cpp
@@ -0,0 +1,65 @@
+/*
+Given a string s, find the longest palindromic subsequence's length in s.
+
+A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
+
+ 
+
+Example 1:
+
+Input: s = "bbbab"
+Output: 4
+Explanation: One possible longest palindromic subsequence is "bbbb".
+Example 2:
+
+Input: s = "cbbd"
+Output: 2
+Explanation: One possible longest palindromic subsequence is "bb".
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists only of lowercase English letters.
+*/
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        string a=s;
+        reverse(s.begin(), s.end());
+        string b=s;
+        
+        int m=a.length();
+        int n=b.length();
+        
+        int dp[m+1][n+1];
+        for(int i=0; i<=m; i++)
+        {
+            for(int j=0; j<=n; j++)
+            {
+                if(i==0 || j==0)
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1; i<=m; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                if(a[i-1]==b[j-1])
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
