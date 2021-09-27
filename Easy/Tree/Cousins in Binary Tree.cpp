05155dc41c846c659866f3f369033a1b8ad36765
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-09-27 11:52:36 +0530

    Create Cousins in Binary Tree.cpp

---
+++ Easy/Tree/Cousins in Binary Tree.cpp
@@ -0,0 +1,79 @@
+/*
+Given the root of a binary tree with unique values and the values of two different nodes of the tree x and y, return true if the nodes corresponding to the values x and y in the tree are cousins, or false otherwise.
+
+Two nodes of a binary tree are cousins if they have the same depth with different parents.
+
+Note that in a binary tree, the root node is at the depth 0, and children of each depth k node are at the depth k + 1.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4], x = 4, y = 3
+Output: false
+Example 2:
+
+
+Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
+Output: true
+Example 3:
+
+
+Input: root = [1,2,3,null,4], x = 2, y = 3
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 100].
+1 <= Node.val <= 100
+Each node has a unique value.
+x != y
+x and y are exist in the tree.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCousins(TreeNode* root, int x, int y) {
+        int parent_x=-1;
+        int parent_y=-1;
+        int lvl_x=-1;
+        int lvl_y=-1;
+        
+        helper(root, x, 0, -1, parent_x, lvl_x);
+        helper(root, y, 0, -1, parent_y, lvl_y);
+        
+        if(lvl_x==lvl_y && parent_x!=parent_y)
+        {
+            return true;
+        }
+        return false;
+    }
+    void helper(TreeNode* root, int req_val, int curr_lvl, int p, int &parent, int &lvl)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        if(root->val==req_val)
+        {
+            parent = p;
+            lvl = curr_lvl;
+            return;
+        }
+        helper(root->left, req_val, curr_lvl+1, root->val, parent, lvl);
+        helper(root->right, req_val, curr_lvl+1, root->val, parent, lvl);
+    }
+};
