0b67d0989a9f76c62456b2349622909164a34a32
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-09-27 11:10:49 +0530

    Create Univalued Binary Tree.cpp

---
+++ Easy/Tree/Univalued Binary Tree.cpp
@@ -0,0 +1,58 @@
+/*
+A binary tree is uni-valued if every node in the tree has the same value.
+
+Given the root of a binary tree, return true if the given tree is uni-valued, or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,1,1,1,1,null,1]
+Output: true
+Example 2:
+
+
+Input: root = [2,2,2,5,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 100].
+0 <= Node.val < 100
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        set<int> st;
+        helper(root, st);
+        if(st.size()>1)
+        {
+            return false;
+        }
+        return true;
+    }
+    void helper(TreeNode* root, set<int> &st)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        st.insert(root->val);
+        helper(root->left, st);
+        helper(root->right, st);
+    }
+};
